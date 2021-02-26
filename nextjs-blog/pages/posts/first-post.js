3a244b3479ab76f027315d91c4f409b15470fe13
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-02-26 23:52:07 +0100

    data fetching

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+        <h1>First Post</h1>
+        <Image
+            src="/images/profile.jpg"
+            height={144}
+            width={144}
+            alt="Your Name"
+        />
+    <h2>
+        <Link href="/">
+            <a>Back to Home</a>
+        </Link>
+    </h2>
+</Layout>
+)
+}
